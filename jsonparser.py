e1d33d3dbbd3dbed55ca64af65aa823646b088a4
Matthias Niel <matthias.niel@student.tugraz.at>
2018-03-19 11:31:25 +0100

    add basic parser functionality. add transcripts for existing talks as raw text

--- jsonparser.py
+++ jsonparser.py
@@ -1,11 +1,36 @@
 import json
 import csv
+import codecs
+import re
 from pprint import pprint
 
-with open('data.csv') as csvfile:
+
+filename = "data - Copy.csv"
+outputFolder = "Transcripts"
+with open(filename) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
+    #Skip the header
     next(readCSV, None)
-    foo = next(readCSV, None)
-    s = foo[8]
-    bar = json.loads(s)
-    pprint(bar)
+    i = 0
+    for row in csvfile:
+        line = next(readCSV, None)
+        oFileName = "transcript_" + line[2]
+        oFileName = re.sub('[<>:"/\|?*.]', '', oFileName)
+        oFileName += ".txt"
+        transcript = line[8]
+        bar = json.loads(transcript)
+        lineStr = ""
+        lineStr.encode('utf-8')
+
+        numParagraphs = len(bar["paragraphs"])
+        for paragraph in range(numParagraphs):
+            numCues = len(bar["paragraphs"][paragraph]["cues"])
+            for cue in range(numCues):
+                foo = bar["paragraphs"][paragraph]["cues"][cue]["text"]
+                foo.encode('utf-8')
+                lineStr += foo
+                lineStr += " "
+        
+        text_file = codecs.open(outputFolder + "\\" + oFileName, "w", "utf-8")
+        text_file.write(lineStr)
+        text_file.close()
